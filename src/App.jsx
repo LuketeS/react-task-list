538cc5370d6bf1b62ace028dcd11224d8dd5d8b5
Lucas de Santana Schmalz <lsschmalz@gmail.com>
2026-02-02 21:58:18 -0300

    upload do projeto

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar React",
+      descripition:
+        "Ler a documentação oficial do React e fazer exercícios práticos.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Inglês",
+      descripition:
+        "Ler a documentação oficial do React e fazer exercícios práticos.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Concurso",
+      descripition:
+        "Ler a documentação oficial do React e fazer exercícios práticos.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
