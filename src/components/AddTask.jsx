f8b5d701f0d24946cd9211accdde1aa4cd326a1e
Lucas de Santana Schmalz <lsschmalz@gmail.com>
2026-02-27 19:09:26 -0300

    feat:adiciona a função de criar novas tarefas

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,34 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [descripition, setDescripition] = useState("");
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <input
         type="text"
         placeholder="Digite o título da tarefa"
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
       ></input>
       <input
         type="text"
         placeholder="Digite o descrição da tarefa"
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={descripition}
+        onChange={(event) => setDescripition(event.target.value)}
       ></input>
-      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+      <button
+        onClick={() => {
+          onAddTaskSubmit(title, descripition);
+          setTitle("");
+          setDescripition("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
         Adicionar
       </button>
     </div>
