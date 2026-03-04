2ac3a60625c610433d6b6ee35f0f2c159c15af78
Lucas de Santana Schmalz <lsschmalz@gmail.com>
2026-03-04 19:13:13 -0300

    feat: instalação do react router e criação da rota para a página principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -48,7 +49,7 @@ function App() {
 
   function onAddTaskSubmit(title, descripition) {
     const newTask = {
-      id: tasks.length + 1,
+      id: v4(),
       title: title,
       descripition: descripition,
       isCompleted: false,
