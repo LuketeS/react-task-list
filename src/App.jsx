eaf99bc8a1ae749e8ed5f53a58883a8ff96c2e85
Lucas de Santana Schmalz <lsschmalz@gmail.com>
2026-03-11 19:29:49 -0300

    feat: Adicionado persistência na lista com localStorage e useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar React",
-      description:
-        "Ler a documentação oficial do React e fazer exercícios práticos.",
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: "Estudar Inglês",
-      description:
-        "Ler a documentação oficial do React e fazer exercícios práticos.",
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: "Estudar Concurso",
-      description:
-        "Ler a documentação oficial do React e fazer exercícios práticos.",
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
+
+  // {
+  //   id: 1,
+  //   title: "Estudar React",
+  //   description:
+  //     "Ler a documentação oficial do React e fazer exercícios práticos.",
+  //   isCompleted: false,
+  // },
+  // {
+  //   id: 2,
+  //   title: "Estudar Inglês",
+  //   description:
+  //     "Ler a documentação oficial do React e fazer exercícios práticos.",
+  //   isCompleted: false,
+  // },
+  // {
+  //   id: 3,
+  //   title: "Estudar Concurso",
+  //   description:
+  //     "Ler a documentação oficial do React e fazer exercícios práticos.",
+  //   isCompleted: false,
+  // },
+
+  // Executa a função da esquerda, sempre que algum valor da lista é alterado.
+  // Irei usar essa funcionalidade para sempre alterar o LocalStorager ao mesmo
+  // tempo que o State das Tasks for alterado
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
